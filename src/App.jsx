a84d19deaa0d7d53be19b70d54a8fc4d32004837
Zhang Hao <vxyu82505@gmail.com>
2025-04-16 11:06:38 +1000

    yeah

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import styled from 'styled-components'
 import GlobalStyles from './components/GlobalStyles'
 import NoteEditor from './components/NoteEditor'
